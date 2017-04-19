002baf3655aab84d6b40454bd47de501b576ef3d
stella kim <stellajkimm@gmail.com>
2017-04-19 13:47:14 -0400

    return external url

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 
-import os
-
+import os, spotipy
 from flask import Flask, request, Response, redirect
 
 app = Flask(__name__)
@@ -18,9 +17,16 @@ def track():
     '''
     text = request.values.get('text')
 
-    resp_qs = ['now playing %s\n' % text]
+    spotify = spotipy.Spotify()
+    results = spotify.search(q='artist:' + text, type='artist', limit=1)
 
-    return Response('\n'.join(resp_qs),
+    if len(results['artists']['items']) > 0:
+        result = results['artists']['items'][0]['external_urls']['spotify']
+    else:
+        result = 'No results for "%s"' % text
+
+
+    return Response(result,
                     content_type='text/plain; charset=utf-8')
 
 if __name__ == "__main__":
