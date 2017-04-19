376c5be126ed86b05a121500169bd4d01d9f42ce
stella kim <stellajkimm@gmail.com>
2017-04-19 18:52:47 -0400

    add login

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import os, json, spotipy
+import spotipy.util as util
 from flask import Flask, request, Response, redirect, jsonify
 
 app = Flask(__name__)
@@ -13,21 +14,23 @@ def index():
 def track():
     '''
     Example:
-        /spotify kendrick lamar humble
+        /spotify loyalty kendrick lamar
+        /spotify artist kendrick lamar
+        /spotify track bitch dont kill my vibe
+        /spotify album damn.
+        /spotify playlist hip hop
     '''
     text = request.values.get('text')
-
     query_types = ['artist', 'track', 'playlist', 'album']
     if text.split(' ', 1)[0] in query_types and len(text.split(' ', 1)) > 1:
         query_type = text.split(' ', 1)[0]
-        query = '{0}:{1}'.format(query_type, text.split(' ', 1)[1])
+        query = text.split(' ', 1)[1]
     else:
         query_type = 'track'
-        query = '{0}:{1}'.format(query_type, text)
+        query = text
 
     spotify = spotipy.Spotify()
     results = spotify.search(q=query, type=query_type, limit=1)
-    print results
     items = results['{}s'.format(query_type)]['items']
 
     if len(items) > 0:
@@ -40,6 +43,19 @@ def track():
         'text': result,
     })
 
+    return data
+
+@app.route('/login', methods=['post'])
+def login():
+    username = request.values.get('text')
+    scope = 'user-read-playback-state'
+    token = util.prompt_for_user_token(username, scope)
+
+    response_text = "Successfully logged in as {}".format(username) if token else "Something went wrong with logging in."
+    data = jsonify({
+        'response_type': 'ephemeral',
+        'text': response_text,
+    })
     return data
 
 if __name__ == "__main__":
