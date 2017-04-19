68d2194552ecd7567f1e6f3c9755f5f8a6ea9710
stella kim <stellajkimm@gmail.com>
2017-04-19 12:35:15 -0400

    change url

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return redirect('https://stellajkimm.github.io')
+    return redirect('https://github.com/stellajkimm/spotify-now-playing')
 
 @app.route('/track', methods=['post'])
 def track():
